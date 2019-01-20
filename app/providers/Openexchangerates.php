9d12bd1743173c6d721e392af0985aa97b07f5d8
Vladimir Chukhalyonock <vchukhalyonock@gmail.com>
2019-01-20 18:45:29 +0200

    Add Openexchangerates provider

---
+++ app/providers/Openexchangerates.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Providers;
+
+use App\Interfaces\IProvider;
+use App\Interfaces\IRequest;
+
+class Openexchangerates implements IProvider {
+
+    const ENDPOINT = 'http://openexchangerates.org/api/latest.json';
+    private $_request;
+
+    public function __construct(IRequest $request) {
+        $this->_request = $request;
+    }
+
+    public function getRate($currency1, $currency2) {
+        $currency1Rate = 0;
+        $currency2Rate = 0;
+
+        try {
+            $response = $this->_getResponse($currency1, $currency2);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+
+        $decodedResponse = json_decode($response, true);
+        if($decodedResponse) {
+            foreach ($decodedResponse['rates'] as $key => $rate) {
+                if($key == $currency1)
+                    $currency1Rate = $rate;
+                if($key == $currency2)
+                    $currency2Rate = $rate;
+            }
+
+            return $currency2Rate / $currency1Rate;
+        } else {
+            return 0;
+        }
+    }
+
+
+    private function _getResponse($currency1, $currency2) {
+        try {
+            $response = $this->_request->getData(
+                self::ENDPOINT,
+                IRequest::METHOD_GET,
+                [
+                    "app_id" => getenv('OPENEXCHANGERATES_API_KEY')
+                ]
+            );
+        } catch (\Exception $e) {
+            throw new \Exception("Cannot get RATE:" . $e->getMessage());
+        }
+
+        return $response;
+    }
+}
